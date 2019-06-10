48f710045f8d6e5b6c0415fe7b827ffab7f7a51e
takatoh <takatoh.m@gmail.com>
2019-06-10 12:34:48 +0900

    main.hs: Define main function.

--- main.hs
+++ main.hs
@@ -1,5 +1,24 @@
+module Main where
+
+import System.Environment ( getArgs )
+
+
 data Cell = Confirmed Int
           | Perhaps [Int]
           deriving (Show, Eq)
 
 type Board = [Cell]
+
+
+main :: IO ()
+main = do argv <- getArgs
+          let board = loadBoard $ head argv
+          printBoard board
+
+
+loadBoard :: String -> Board
+loadBoard = undefined
+
+
+printBoard :: Board -> IO ()
+printBoard = undefined
