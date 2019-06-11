59a4d1200bf11d13713a0543729efc4264055f33
takatoh <takatoh.m@gmail.com>
2019-06-11 13:31:16 +0900

    main.hs: --version and --help options.

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment ( getArgs )
 import Data.Char
 import Data.List
+import System.Console.GetOpt
 
 --------------------------------------------------------------------------------
 
@@ -14,13 +15,53 @@ type Board = [[Cell]]
 
 --------------------------------------------------------------------------------
 
+progName = "sudoku"
+version  = "0.1.0"
+
+
 main :: IO ()
 main = do argv <- getArgs
-          cs <- readFile $ head argv
-          let board = parseBoard cs
-          case (solv board) of
-            Just b  -> printBoard b
-            Nothing -> putStrLn "Failed to solve."
+          (o, n) <- parseArgs argv
+          if optShowVersion o then
+            putStrLn version
+          else if optShowHelp o then
+            putStrLn $ usageInfo header options
+          else do cs <- readFile $ head n
+                  let board = parseBoard cs
+                  case (solv board) of
+                    Just b  -> printBoard b
+                    Nothing -> putStrLn "Failed to solve."
+
+--------------------------------------------------------------------------------
+
+-- command line options
+
+data Options = Options { optShowVersion :: Bool
+                       , optShowHelp    :: Bool
+                       }
+
+defaultOptions = Options { optShowVersion = False
+                         , optShowHelp    = False
+                         }
+
+options :: [OptDescr (Options -> Options)]
+options = [ Option ['v']      ["version"]
+            (NoArg (\ opts -> opts { optShowVersion = True}))
+            "show version"
+          , Option ['h', '?'] ["help"]
+            (NoArg (\ opts -> opts { optShowHelp = True}))
+            "show this message"
+          ]
+
+
+header :: String
+header = "Usage: " ++ progName ++ " [OPTION] <input>\n\nOptions:"
+
+
+parseArgs :: [String] -> IO (Options, [String])
+parseArgs argv = case getOpt Permute options argv of
+                   (o,n,[])   -> return (foldl (flip id) defaultOptions o, n)
+                   (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
 
 --------------------------------------------------------------------------------
 
