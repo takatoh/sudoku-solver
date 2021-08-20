fbebcdeb47d527a349da370ea47a3cb7465427b5
takatoh <takatoh.m@gmail.com>
2021-08-20 20:12:13 +0900

    src/Lib.hs: Tweak.

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,6 +9,7 @@ data Cell = Confirmed Int
           | Perhaps [Int]
           deriving (Show, Eq)
 
+
 type Board = [[Cell]]
 
 --------------------------------------------------------------------------------
